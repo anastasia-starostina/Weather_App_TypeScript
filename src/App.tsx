9e649b53c78bd7dedd84dc19452b8912e25189de
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-08-26 18:08:28 +0100

    Refactor Kelvin to Celcius units and add styles to css

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,67 @@
-import React from 'react';
-import './App.css';
-import Description from './Components/Description/Description';
-import Header from './Components/Header/Header';
-import WeatherDisplay from './Components/WeatherDisplay/WeatherDisplay';
-import {useEffect, useState} from 'react';
-import Input from './Components/Input/Input';
-import Button from './Components/Button/button';
-
+import React from "react";
+import "./App.css";
+import Description from "./Components/Description/Description";
+import Header from "./Components/Header/Header";
+import WeatherDisplay from "./Components/WeatherDisplay/WeatherDisplay";
+import { useEffect, useState } from "react";
+import Input from "./Components/Input/Input";
+import Button from "./Components/Button/button";
 
 function App() {
-
   let data = {
     name: "",
     weather: [
       {
         icon: "",
-        description:""
-      }
-    ], 
-    main:{
-      temp:0,
-      feels_like:0
+        description: "",
+      },
+    ],
+    main: {
+      temp: 0,
+      feels_like: 0,
     },
-    wind:{
-      speed:0
-    }
-  }
+    wind: {
+      speed: 0,
+    },
+  };
 
-  const [city, setCity] = useState("")
-  const [inputCity, setInputCity] = useState("London,uk")
-  console.log(city)
+  const [city, setCity] = useState("");
+  const [inputCity, setInputCity] = useState("London,uk");
+  console.log(city);
 
   function submitCity() {
-    setInputCity(city)
+    setInputCity(city);
   }
 
-  const [weatherData, setWeatherData] = useState(data)
-  console.log(weatherData)
-  useEffect(()=> {  
-     async function getWeatherData () {
-      const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&APPID=8aba6173163790333e212dc3935b4c03`);
+  const [weatherData, setWeatherData] = useState(data);
+  console.log(weatherData);
+  useEffect(() => {
+    async function getWeatherData() {
+      const result = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&APPID=8aba6173163790333e212dc3935b4c03`
+      );
       const answer = await result.json();
       console.log(answer);
       setWeatherData(answer);
-
     }
-    getWeatherData()
-    console.log(weatherData)
-  }, [inputCity] )
-
-  //
-
-
-  // useEffect(()=> {
-  //   async function submitCity(city: string) {
-  //     const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=8aba6173163790333e212dc3935b4c03`);
-  //     const answer = await result.json();
-  //     console.log(answer);
-  //     setWeatherData(answer)
-  //   }
-  // }, [])
-  
-
-  //set up state
-  // - import useState +++
-  // - weather, setWeather +++
-  //send a fetch request to API  +++
-  //What we are fetching: city, icon, temp, feels_like, windspeed, description
-  //console.log what we are fethcing
-  //pass result to result.json
-  //result of result.json => setWeatherData as a response object
-  //state will be weather and we'll pull data from the object
-
+    getWeatherData();
+    console.log(weatherData);
+  }, [inputCity]);
 
   return (
-    
     <div className="App">
-      <Input text={setCity}/>
-      <Button onClick={submitCity}/>
-      <Header city={weatherData.name}/>
-      <WeatherDisplay icon={weatherData.weather[0].icon} temp={weatherData.main.temp}  feelsLike={weatherData.main.feels_like} windspeed={weatherData.wind.speed}/>
-      <Description text={weatherData.weather[0].description}/>
+      <section className="input__form">
+        <Input text={setCity} />
+        <Button onClick={submitCity} />
+      </section>
+      <Header city={weatherData.name} />
+      <WeatherDisplay
+        icon={weatherData.weather[0].icon}
+        temp={Math.round(weatherData.main.temp - 273.15) + "°"}
+        feelsLike={Math.round(weatherData.main.feels_like - 273.15) + "°"}
+        windspeed={weatherData.wind.speed + 'm/s'}
+      />
+      <Description text={weatherData.weather[0].description} />
     </div>
   );
 }
--- src/Components/WeatherDisplay/WeatherDisplay.tsx
+++ src/Components/WeatherDisplay/WeatherDisplay.tsx
@@ -1,28 +1,30 @@
-import DataDisplay from "./Displays/DataDisplay"
-import "./WeatherDisplay.css"
+import DataDisplay from "./Displays/DataDisplay";
+import "./WeatherDisplay.css";
 
 type displayProps = {
-    id?: string
-    icon: any
-    temp: any 
-    feelsLike: any
-    windspeed: any
+  id?: string;
+  icon: any;
+  temp: any;
+  feelsLike: any;
+  windspeed: any;
+};
+
+function WeatherDisplay(props: displayProps) {
+  return (
+    <div className="displayContainer">
+      <section className="picture">
+        <DataDisplay id="iconDisplay" display={props.icon} />
+      </section>
+      <section className="data">
+        <h5>Temperature</h5>
+        <DataDisplay id="tempDisplay" display={props.temp} />
+        <h5>Feels Like</h5>
+        <DataDisplay id="feelsLikeDisplay" display={props.feelsLike} />
+        <h5>Wind Speed</h5>
+        <DataDisplay id="windspeedDisplay" display={props.windspeed} />
+      </section>
+    </div>
+  );
 }
 
-function WeatherDisplay(props: displayProps){
-return (
-<div className="displayContainer">
-    <section className="picture">
-        <DataDisplay id="iconDisplay" display={props.icon}/>
- </section>
-   <section className="data">
-    <DataDisplay id="tempDisplay" display={props.temp}/>
-    <DataDisplay id="feelsLikeDisplay" display={props.feelsLike}/>
-    <DataDisplay id="windspeedDisplay" display={props.windspeed}/>
-    </section>
-    
-</div>
-)
-}
-
-export default WeatherDisplay
+export default WeatherDisplay;
